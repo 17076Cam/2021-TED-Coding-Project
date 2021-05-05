7e8f3e4c885582920541625d8c286163d192ee71
17076Cam <63985024+17076Cam@users.noreply.github.com>
2021-05-05 13:22:07 +1200

    Add files via upload

---
+++ EnviroQuizGUI1.0.py
@@ -0,0 +1,61 @@
+# Environment GUI quiz
+# Cameron Galbraith
+# Version 1.0
+"""
+
+make a program that can quiz the user
+@copyright 2021 Cameron Galbraith <17076@rangiorahigh.school.nz>
+"""
+# imports
+from appJar import gui
+
+
+
+# variables
+
+# class
+class GUI:
+    # Create GUI
+    def __init__(self):
+        app = gui("Enviro Quiz", "800x500")
+        app.setBg("DarkSlateGray4")
+        app.setFont(18)
+        with app.frame("Main"):
+            app.addLabel("Enviro Quiz", colspan= 4, row = 0, column= 0)
+            app.setLabelBg("Enviro Quiz", "blue")
+            app.addButton("Start", self.start_quiz, colspan=1, row=7, column=3)
+            app.addButton("Close", self.close_quiz, colspan=1, row=7, column=0)
+            app.setAllButtonWidths(7)
+
+        with app.frame("questions"):
+            app.addLabel("Question here", colspan=4, row=0, column=0)
+            app.hideFrame("questions")
+
+
+
+        self._app = app
+        self._app.go()
+
+    def start_quiz(self):
+        self._app.hideFrame("Main")
+        self._app.showFrame("questions")
+
+    def close_quiz(self):
+        pass
+
+# main
+
+
+
+
+
+
+window = GUI()
+window.start.app()
+
+
+#can add later
+'''
+app.addMeter("progress")
+app.setMeterFill("progress", "blue")
+'''
